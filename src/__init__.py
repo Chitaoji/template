5048394a5a29f29c1251260bf5a7c101d911198b
yifanpan <2360742040@qq.com>
2024-07-23 14:09:09 +0800

    Bugfix for setup.py

--- src/__init__.py
+++ src/__init__.py
@@ -15,7 +15,11 @@ This project falls under the BSD 3-Clause License.
 
 """
 
+from typing import List
+
+from . import core
 from .__version__ import __version__
 from .core import *
 
-__all__ = []
+__all__: List[str] = []
+__all__.extend(core.__all__)
