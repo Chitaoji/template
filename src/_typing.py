8bc967db5e5b5efdc990edb7132c8e9b7190cff6
Chitaoji <2360742040@qq.com>
2025-05-27 22:16:17 +0800

    Update log

--- src/_typing.py
+++ src/_typing.py
@@ -1,14 +1,14 @@
 """
 Contains typing classes.
 
-NOTE: this module is private. All functions and objects are available in the main
-`$package` namespace - use that instead.
+NOTE: this module is not intended for direct import.
 
 """
 
 import logging
 
-logging.warning(
-    "importing from '._typing' - this module is not intended for direct import, "
-    "therefore unexpected errors may occur"
+logging.getLogger(__name__).warning(
+    "importing from %s - this module is not intended for direct import, "
+    "therefore unexpected errors may occur",
+    __name__,
 )
