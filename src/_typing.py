7b20df2876be022e0c4afd99c87172d28cafbc30
Chitaoji <2360742040@qq.com>
2025-05-27 22:20:00 +0800

    Update log

--- src/_typing.py
+++ src/_typing.py
@@ -1,14 +1,14 @@
 """
 Contains typing classes.
 
-NOTE: this module is not intended for direct import.
+NOTE: this module is not intended to be directly imported.
 
 """
 
 import logging
 
 logging.getLogger(__name__).warning(
-    "importing from '%s' - this module is not intended for direct import, "
+    "importing from '%s' - this module is not intended to be directly imported, "
     "therefore unexpected errors may occur",
     __name__,
 )
