ae311d04696fbc19251bb9421a119fcda5833fe6
Chitaoji <2360742040@qq.com>
2025-05-27 22:16:58 +0800

    Update log

--- src/_typing.py
+++ src/_typing.py
@@ -8,7 +8,7 @@ NOTE: this module is not intended for direct import.
 import logging
 
 logging.getLogger(__name__).warning(
-    "importing from %s - this module is not intended for direct import, "
+    "importing from '%s' - this module is not intended for direct import, "
     "therefore unexpected errors may occur",
     __name__,
 )
